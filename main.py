77cb891250c88c9b93048d78e82a6e24f8b88ece
gicha <01gicha01@gmail.com>
2017-02-20 21:21:11 +0300

    cool potc test

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import config
 import telebot
+
+import config
 
 bot = telebot.TeleBot(config.tocken)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): 
-    bot.send_message(message.chat.id, message.text)
+def repeat_all_messages(message):
+    if message.text == 'Я клевый поц?':
+        bot.send_message(message.chat.id, 'Да!')
+    else:
+        bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
