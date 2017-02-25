884ff78e848522c04d48eca22701c487f21e65d2
MrPoliteGrizly <lbekehack@gmail.com>
2017-02-26 01:17:18 +0600

    Add Flask to main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*- 
 import telebot
 
+from flask import Flask, request
+
 import config
 
 bot = telebot.TeleBot(config.tocken)
+server = Flask(__name__)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
@@ -12,6 +15,8 @@ def repeat_all_messages(message):
     else:
         bot.send_message(message.chat.id, message.text)
 
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
     print(bot.get_me())
